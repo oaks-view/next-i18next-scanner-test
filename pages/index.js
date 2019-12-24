9d191c299ae32777db2018bdfa015fec04364842
Moses igbuku <captainoak@gmail.com>
2019-12-24 18:13:39 +0100

    identified error issues

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,14 @@ import { useTranslation } from '@root/i18n';
 
 const Index = () => {
   const { t } = useTranslation();
-  return (
-  <h1>{t('home-h1')}</h1>
+  const marks = Object.values(t('journey.step.area.marks', { returnObjects: true, defaultValue: {} })).map(mark => ({
+    ...mark,
+    value: Number.parseInt(mark.value, 10),
+  }));
+  return (<>
+    <h1>{t('home-h1')}</h1>
+    <h2>{marks}</h2>
+  </>
   );
 }
 
